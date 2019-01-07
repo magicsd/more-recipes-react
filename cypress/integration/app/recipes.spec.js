a9758129491070f7549324e846c8b23b3beb4523
Alex <sd@alexdus.com>
2019-01-07 16:35:54 +0300

    Refactor add recipes test user signin

--- cypress/integration/app/recipes.spec.js
+++ cypress/integration/app/recipes.spec.js
@@ -1,33 +1,37 @@
 import faker from 'faker';
 
-const baseUrl = 'http://localhost:5678/';
+const baseUrl = 'http://localhost:5678';
 
 const generateRecipe = () => ({
   title: faker.lorem.sentence(),
-  description: faker.lorem.sentences(4),
+  description: faker.lorem.sentences(3),
   timeToCook: faker.random.number(),
   ingredients: [faker.lorem.sentence(), faker.lorem.sentence()],
   procedure: [faker.lorem.sentence(), faker.lorem.sentence()],
 });
 
+const generateUser = () => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
+
 describe('Recipe Creation Process', () => {
+  let fakeUser;
+
+  beforeEach(() => {
+    fakeUser = generateUser();
+
+    cy.request('POST', `${baseUrl}/api/v1/users/signup`, fakeUser)
+      .then(({ body }) => {
+        cy.window().then(window => {
+          window.localStorage.setItem('authUser', JSON.stringify(body));
+        })
+      });
+  });
+
   it('Should create a recipe for the user', () => {
     cy.visit(baseUrl);
-    cy.get('[href="/auth/register"]').click();
-
-    const fakeUser = {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-
-    cy.get('input[name="name"]').type(fakeUser.name);
-    cy.get('input[name="email"]').type(fakeUser.email);
-    cy.get('input[name="password"]').type(fakeUser.password);
-    cy.get('input[name="confirmPassword"]').type(fakeUser.password);
-    cy.get('.btn').click();
-
-    cy.url().should('equal', baseUrl);
 
     cy.get('[data-testid=createRecipeHome]').click();
 
@@ -53,6 +57,6 @@ describe('Recipe Creation Process', () => {
     cy.contains(fakeRecipe.ingredients[1]);
     cy.contains(fakeRecipe.procedure[0]);
     cy.contains(fakeRecipe.procedure[1]);
-  
+
   });
 });
